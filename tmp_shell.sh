7ede685f0dbae1f347564465b61bdcbe3dd1655c
LeGlowik5537! <remi.montagut@tessi.fr>
2024-03-04 16:45:27 +0100

    Add file

--- tmp_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exécute la commande awk pour extraire l'ID
-id=$(awk '{gsub(/\\/, "\\\\");} $4 == "C:\\Users\\ansible\\Desktop\\Version_1\\RevoUn.exe" {print $3}' /home/ansible/tmp/output.txt)
-
-# Affiche le résultat
-echo "ID: $id"
